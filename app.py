d3ad3ab3e9e09f8480791a2347c14095413b62c5
yashwanth491 <129652030+yashwanth491@users.noreply.github.com>
2025-02-15 22:58:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ if st.button("Review Code"):
             st.markdown(feedback)  
     else:
         st.warning("⚠️ Please enter some Python code before submitting.")
-st.markdown("<br><hr><h3 style='text-align: center;'>👨‍💻 Made by <b>Yashwanth</b></h3><hr><br>", unsafe_allow_html=True)
+st.markdown("<hr><h5 style='text-align: center;'>👨‍💻 Made by <b>Yashwanth</b></h5><hr>", unsafe_allow_html=True)
 
